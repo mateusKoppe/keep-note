8e6ec9f2013443dc379a51db4983d047c397eaab
Mateus Koppe <mateus.koppe@gmail.com>
2020-05-31 15:59:42 -0300

    Reset app

--- src/App.js
+++ src/App.js
@@ -1,73 +1,10 @@
-import React, { Component } from 'react';
-import CardList from './CardList';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      cardList: [],
-      form: {
-        title: '',
-        description: ''
-      }
-    }
-  }
-
-  cardSubmitHandle = (e) => {
-    this.addCard({
-      ...this.state.form
-    });
-    e.preventDefault();
-  }
-
-  changeField = (e) => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value
-      }
-    })
-  }
-
-  addCard = (card) => {
-    this.setState({
-      form: {
-        title: '',
-        description: ''
-      },
-      cardList: this.state.cardList.concat([card]) 
-    })
-  }
-
-  onChangeList = (newList) => {
-    this.setState({
-      cardList: newList 
-    }) 
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Note List</h1>
-        <form
-          onSubmit={this.cardSubmitHandle}
-          className="App-search-form"
-        >
-          <input
-            name="title"
-            className="App-search-input"
-            value={this.state.form.title}
-            onChange={this.changeField}
-          />
-        </form>
-        <CardList
-          list={this.state.cardList}
-          onChange={this.onChangeList}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
 export default App;
